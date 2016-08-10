0d344b8054af228db48f0cf01bed6a086ad8f590
ndyumin <nikita.dyumin@gmail.com>
2016-08-10 14:27:28 +0500

    update renderer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
-import {view, model} from './vtable';
 import {mock, meta, shuffle} from './mockups';
 import Rx from 'rxjs';
 import rstore from 'rstore';
 import h from 'snabbdom/h';
 
+import {view} from './vtable';
+import {model} from './model';
+import Renderer from  './renderer';
+
+class CustomRenderer extends Renderer {
+    renderDataCell({column, value}) {
+        return h('td', [
+            column.name === 'A' ? value.toUpperCase() : value
+        ]);
+    }
+    renderHeaderCell(column) {
+        return h('td', [
+            column.name === 'D' ? h('b', ['sdfsdf']) : column.name
+        ]);
+    }
+}
+
 const data$ = Rx.Observable.of(mock());
 
-const meta_ = meta();
-meta_.columns[4].dataCellRenderer = value => h('td', value.toUpperCase());
-meta_.columns[3].headerCellRenderer = value => h('td', value.toLowerCase());
-
 const meta$ = Rx.Observable.interval(500)
-    .map(() => ({columns: shuffle(meta_.columns)}));
+    .map(() => ({columns: shuffle(meta().columns)}));
 
 const $container = document.getElementById('table');
-view($container, model(meta$, data$));
+const renderer = new CustomRenderer();
+view($container, renderer, model(meta$, data$));
 
 
 
--- src/vtable.js
+++ src/vtable.js
@@ -2,13 +2,9 @@
  * Created by ndyumin on 09.08.2016.
  */
 import Rx from 'rxjs';
-import {curry} from 'ramda';
 import snabbdom from 'snabbdom';
-import h from  'snabbdom/h';
-import {storeRx, lens} from 'rstore';
 
-const dataL = lens('data');
-const metaL = lens('meta');
+import {EMPTY_MODEL} from './model';
 
 const patch = snabbdom.init([
     require('snabbdom/modules/class'),
@@ -17,67 +13,10 @@ const patch = snabbdom.init([
     require('snabbdom/modules/eventlisteners')
 ]);
 
-/**
- *
- * column API
- *
- * - string id
- * - string name
- * - function dataCellRenderer
- * - function headerCellRenderer
- *
- */
-
-const emptyModel = {
-    meta: {
-        columns: []
-    },
-    data: {
-        items: [],
-        count: 0
-    }
-};
-
-export function model(meta$, dataSource$) {
-    return storeRx(emptyModel).plug(
-        meta$, metaL.set,
-        dataSource$, dataL.set
-    );
-}
-
-const isVisible = c => c.visible;
-const methodOrFunction = curry((key, obj, func) =>
-    typeof obj[key] === 'function' ? obj[key] : func);
-
-const customRendererIfAvailable = methodOrFunction('dataCellRenderer');
-const customHeaderRendererIfAvailable = methodOrFunction('headerCellRenderer');
-
-const textCellRenderer = value => h('td.text-cell', value);
-
-const renderHeaderCell = c => customHeaderRendererIfAvailable(c, textCellRenderer)(c.name);
-const renderDataCell = ({column, value}) => customRendererIfAvailable(column, textCellRenderer)(value);
-
-const renderHeader = model => h('thead', [
-    h('tr', model.meta.columns.filter(isVisible).map(renderHeaderCell))
-]);
-
-const renderBody = model => h('tbody',
-    model.data.items.map(entry =>
-        h('tr',
-            model.meta.columns
-                .filter(isVisible)
-                .map(column => ({column, value: entry[column.id]}))
-                .map(renderDataCell))));
-
-const render = model => h('table.vtable', [
-    renderHeader(model),
-    renderBody(model)
-]);
-
-export function view($container, model$) {
-    let vnode = render(emptyModel);
+export function view($container, renderer, model$) {
+    let vnode = renderer.render(EMPTY_MODEL);
     patch($container, vnode);
     return model$.subscribe(function (model) {
-        vnode = patch(vnode, render(model));
+        vnode = patch(vnode, renderer.render(model));
     });
 }
